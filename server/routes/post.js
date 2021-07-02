9b42d8dd39228bcbd5ffd8cf224907e238b49da9
Aaquib Ahmed <aaquibskimbox@gmail.com>
2021-07-02 22:01:50 +0530

    get method is setuped

--- server/routes/post.js
+++ server/routes/post.js
@@ -1,9 +1,11 @@
 import express from 'express'
+import { getPost, createPost } from '../controller/posts.js'
+
+
 
 const router = express.Router()
 
-router.get('/', (req,res) => {
-    res.send('it works!!')
-})
+router.get('/', getPost)
+router.post('/', createPost)
 
 export default router;
