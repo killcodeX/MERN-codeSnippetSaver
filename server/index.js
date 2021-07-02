bc4c622f0f08d23a5cd6e320ca016a928459db6d
Aaquib Ahmed <aaquibskimbox@gmail.com>
2021-07-02 20:35:13 +0530

    connected to data base

---
+++ server/index.js
@@ -0,0 +1,25 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
+import cors from 'cors';
+
+
+
+const app = express()
+
+app.use(bodyParser.json({limit: "30mb", extended: true}))
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}))
+app.use(cors())
+
+
+// for database
+
+const connectionUrl = "mongodb+srv://Aaquib5076:Aaquib5076@cluster0.4fa9k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(connectionUrl, {useUnifiedTopology : true, useNewUrlParser : true})
+    .then(() => app.listen(PORT, () => console.log(`Server running on port : ${PORT}`)))
+    .catch(err => console.log(err))
+
+mongoose.set('useFindAndModify', false)
