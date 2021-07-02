a33dffa771e23ab0d9b5437f7e7eeaceb6007165
Aaquib Ahmed <aaquibskimbox@gmail.com>
2021-07-02 22:09:23 +0530

    post method is setuped

--- server/controller/posts.js
+++ server/controller/posts.js
@@ -12,6 +12,13 @@ export const getPost = async (req,res) => {
 }
 
 
-export const createPost = (req,res) => {
-    res.send('Post created!!')
+export const createPost = async (req,res) => {
+    const body = req.body;
+    const newPost = new PostMessage(body)
+    try{
+        await newPost.save()
+        res.status(201).json(newPost)
+    } catch(error){
+        res.status(404).json({ message: error.message})
+    }
 }
