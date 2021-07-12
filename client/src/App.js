9963e4511819b0f1608d81ba520143621f21f051
Aaquib Ahmed <aaquibskimbox@gmail.com>
2021-07-12 13:33:49 +0530

    login is setuped

--- client/src/App.js
+++ client/src/App.js
@@ -5,37 +5,32 @@ import { Header, SideMenu } from "./layout";
 import { lightTheme, darkTheme } from "./layout/theme";
 import { Home, Login, Register } from "./components";
 import { useSelector, useDispatch } from "react-redux";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  useHistory,
-} from "react-router-dom";
+import { Switch, Route, useHistory } from "react-router-dom";
 import { verifyStorage } from "./redux/actions/actions";
 
 function App() {
-  const history = useHistory();
+  let history = useHistory();
   const dispatch = useDispatch();
   const theme = useSelector((state) => state.Codes.theme);
   const isAuthenticated = useSelector((state) => state.Auth.isAuthenticated);
-  const isVerifying = useSelector((state) => state.Auth.isVerifying);
+  console.log("history -->", history);
   useEffect(() => {
     dispatch(verifyStorage());
-    // if (!isAuthenticated) {
-    //   history.pushState("/login");
-    // }
+    if (!isAuthenticated) {
+      history.push("/login");
+    } else{
+      history.push('/')
+    }
   }, [isAuthenticated]);
   return (
-    <Router>
-      <ThemeProvider theme={theme === false ? darkTheme : lightTheme}>
-        <Header />
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/login" exact component={Login} />
-          <Route path="/register" exact component={Register} />
-        </Switch>
-      </ThemeProvider>
-    </Router>
+    <ThemeProvider theme={theme === false ? darkTheme : lightTheme}>
+      <Header />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/login" exact component={Login} />
+        <Route path="/register" exact component={Register} />
+      </Switch>
+    </ThemeProvider>
   );
 }
 
